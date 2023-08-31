2e004b801b6fbb611f89262a65976746dd026405
Dinobi <architectstan01@gmail.com>
2023-09-01 00:06:00 +0100

    Filtered logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,8 +3,9 @@
 from typing import List
 import re
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str):
-    """Returns a obfuscated log message"""
+    """ A function that 
+    Returns a obfuscated log message"""
     for i in fields:
-        message = re.sub(i+'=' + '.*?'+separator, 
-                         i+ '=' + redaction + separator, message)
+        message = re.sub(i +'=' + '.*?'+ separator, 
+                         i + '=' + redaction + separator, message)
     return message
