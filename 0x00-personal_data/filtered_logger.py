cef26658268f5581126acad394824612c6b83628
Dinobi <architectstan01@gmail.com>
2023-09-01 00:10:51 +0100

    Filtered logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,10 +2,26 @@
 """Personal data"""
 from typing import List
 import re
+import logging
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str):
     """ A function that 
     Returns a obfuscated log message"""
     for i in fields:
-        message = re.sub(i +'=' + '.*?'+ separator, 
-                         i + '=' + redaction + separator, message)
+        message = re.sub(i + "=" + ".*?"+ separator, 
+                         i + "=" + redaction + separator, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        
