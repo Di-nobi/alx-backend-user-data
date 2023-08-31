40f4fb74e3ca5397d90b588b25e4dce4ec84e69f
Dinobi <architectstan01@gmail.com>
2023-09-01 00:45:23 +0100

    Personal data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,16 +13,18 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     return message
 
 
-# class RedactingFormatter(logging.Formatter):
-#     """ Redacting Formatter class
-#         """
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
 
-#     REDACTION = "***"
-#     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-#     SEPARATOR = ";"
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
 
-#     def __init__(self):
-#         super(RedactingFormatter, self).__init__(self.FORMAT)
-
-#     def format(self, record: logging.LogRecord) -> str:
-        
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+    def format(self, record: logging.LogRecord) -> str:
+        """Formatting of messages"""
+        oncall = super(RedactingFormatter, self).format(record)
+        return filter_datum(self.fields, self.REDACTION, oncall, self.SEPARATOR)
