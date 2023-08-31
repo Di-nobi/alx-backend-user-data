79d1c1d92282c507aa7b69d6c493fd795387ea22
Dinobi <architectstan01@gmail.com>
2023-08-31 23:59:46 +0100

    Filtered logger

---
+++ 0x00-personal_data/flitered_logger.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Personal data"""
+from typing import List
+import re
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str):
+    """Returns a obfuscated log message"""
+    for i in fields:
+        message = re.sub(i+'=' + '.*?'+separator, 
+                         i+ '=' + redaction + separator, message)
+    return message
