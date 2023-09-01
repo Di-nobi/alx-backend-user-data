899f1eac2cd492bcc1d36e6df54a349d703db3b2
Dinobi <architectstan01@gmail.com>
2023-09-01 21:39:47 +0100

    Backend User Data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,3 +6,7 @@ def hash_password(password: str) -> bytes:
     """Encodes password"""
     hash_password = bcrypt.hashpw( password.encode('utf-8'), bcrypt.gensalt())
     return hash_password
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validates a password"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
