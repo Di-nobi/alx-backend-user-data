ec22c7dda33bd7e2ee1dee69a72b400c8ebeae59
Dinobi <architectstan01@gmail.com>
2023-09-05 10:09:48 +0100

    User Auth Data

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,9 +8,7 @@ class BasicAuth(Auth):
     """Basic Authentication that inherits from Auth"""
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
         """Extraction of base64 authorization"""
-        if authorization_header is None :
-            return None
-        if type(authorization_header) is not str:
+        if authorization_header is None or type(authorization_header) is not str:
             return None
         if not authorization_header.startswith("Basic "):
             return None
