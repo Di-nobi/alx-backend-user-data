8f4d67ace455d8cce4ea9ed9f4db9625aec55590
Dinobi <architectstan01@gmail.com>
2023-09-09 22:37:27 +0100

    User data

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -3,20 +3,24 @@
 from models.user import User
 from api.v1.auth.session_auth import SessionAuth
 import os
-import datetime
+#import datetime
+from datetime import datetime, timedelta
 class SessionExpAuth(SessionAuth):
     """Session Expiration Class Instance"""
     def __init__(self):
         self.session_duration = int(os.getenv('SESSION_DURATION'))
-        if type(self.session_duration) != int:
+        if not self.session_duration or type(self.session_duration) != int:
             self.session_duration = 0
+
     def create_session(self, user_id=None):
         """Creating of a SESSION ID"""
         session = super().create_session(user_id)
         if not session:
             return None
-        self.user_id_by_session_id[session] = {"user_id": user_id, "created_at": datetime.now() }
+        session_dictionary = {"user_id": user_id, "created_at": datetime.now()}
+        self.user_id_by_session_id[session] = session_dictionary
         return session
+    
     def user_id_for_session_id(self, session_id=None):
         """Gets a User for its session id"""
         if not session_id:
@@ -28,4 +32,6 @@ class SessionExpAuth(SessionAuth):
             return None
         if self.session_duration <= 0:
             return details.get("user_id")
-        
+        if details.get("created_at") + timedelta(seconds=self.session_duration) <= datetime.now():
+            return None
+        return details.get("user_id")
