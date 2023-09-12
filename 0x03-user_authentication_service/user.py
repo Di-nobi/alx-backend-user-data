26118a1bb4f309beb477407d7b202abd024d4d6b
Dinobi <architectstan01@gmail.com>
2023-09-12 17:33:26 +0100

    User data update

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -9,7 +9,7 @@ class User(Base):
     """Users class"""
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    email = Column(String(60), nullable=False)
-    hashed_password = Column(String(60), nullable=False)
-    session_id = Column(String(60), nullable=True)
-    reset_token = Column(String(60), nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
