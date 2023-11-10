6655ffd4224311954d2b0494e701916aacd8c2ab
Dinobi <architectstan01@gmail.com>
2023-11-10 15:56:03 +0100

    Authentication

---
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""Database """
+
+from models.user_session import UserSession
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from datetime import datetime, timedelta
+
+
+class SessionDBAuth(SessionExpAuth):
+    """Database class Authentication"""
+
+    def create_session(self, user_id=None):
+        """Creates and stores new instance of UserSession 
+        and returns the Session ID"""
+        session_data = super().create_session(user_id)
+        if not session_data:
+            return None
+        myList = {
+            "user_id": user_id,
+            "session_id": session_data
+        }
+        usr_session = UserSession(**myList)
+        usr_session.save()
+        return session_data
+    
+    def user_id_for_session_id(self, session_id=None):
+        """"Gets the user id"""
+        usr = UserSession.search({'session_id': session_id})
+        if not usr:
+            return None
+        current = datetime.now()
+        expire_time = usr[0].created_at + timedelta(seconds=self.session_duration)
+        if expire_time < current:
+            return None
+        return usr[0].user_id
+    
+    def destroy_session(self, request=None):
+        """Destroys the current session"""
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return False
+        sessions = UserSession.search({'session_id': session_id})
+        if not sessions:
+            return False
+        if len(sessions) <= 0:
+            return False
+        sessions[0].remove()
+        return True
+    
